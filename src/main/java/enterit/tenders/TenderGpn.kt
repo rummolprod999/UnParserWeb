412ff509827bc14242a1c5ce83f7e9fae3573ca5
rummolprod999 <rummolprod999@gmail.com>
2018-02-07 17:17:51 +0300

    update

--- src/main/java/enterit/tenders/TenderGpn.kt
+++ src/main/java/enterit/tenders/TenderGpn.kt
@@ -1,27 +1,193 @@
 package enterit.tenders
 
-import enterit.downloadFromUrl
-import enterit.formatterGpn
-import enterit.getDateFromFormat
-import enterit.logger
+import enterit.*
 import org.jsoup.Jsoup
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.Statement
+import java.sql.Timestamp
+import java.util.*
 
 class TenderGpn(val status: String, val url: String) {
     companion object TypeFz {
-        val typeFz = 12
+        val typeFz = 13
     }
 
-    fun parsing(){
+    fun parsing() {
         val stPage = downloadFromUrl(url)
         if (stPage == "") {
             logger("Gets empty string TenderGpn", url)
             return
         }
         val html = Jsoup.parse(stPage)
-        val startDateT = html.selectFirst("div:contains(Дата и время начала приёма предложений:) + div")?.ownText()?.trim { it <= ' ' } ?: ""
-        val endDateT = html.selectFirst("div:contains(Дата и время окончания приёма предложений:) + div")?.ownText()?.trim { it <= ' ' } ?: ""
+        val startDateT = html.selectFirst("div:contains(Дата и время начала приёма предложений:) + div")?.ownText()?.trim { it <= ' ' }
+                ?: ""
+        val endDateT = html.selectFirst("div:contains(Дата и время окончания приёма предложений:) + div")?.ownText()?.trim { it <= ' ' }
+                ?: ""
         val startDate = getDateFromFormat(startDateT, formatterGpn)
         val endDate = getDateFromFormat(endDateT, formatterGpn)
-        println(startDate)
+        if (startDate == Date(0L)) {
+            logger("Empty start date in $url")
+            return
+        }
+        val purNum = html.selectFirst("div:contains(Реестровый номер:) + div")?.ownText()?.trim { it <= ' ' } ?: ""
+        if (purNum == "") {
+            logger("Empty purchase number in $url")
+            return
+        }
+        DriverManager.getConnection(UrlConnect, UserDb, PassDb).use(fun(con: Connection) {
+            val stmt0 = con.prepareStatement("SELECT id_tender FROM ${Prefix}tender WHERE purchase_number = ? AND date_version = ? AND type_fz = ? AND notice_version = ? AND end_date = ?")
+            stmt0.setString(1, purNum)
+            stmt0.setTimestamp(2, Timestamp(startDate.time))
+            stmt0.setInt(3, typeFz)
+            stmt0.setString(4, status)
+            stmt0.setTimestamp(5, Timestamp(endDate.time))
+            val r = stmt0.executeQuery()
+            if (r.next()) {
+                r.close()
+                stmt0.close()
+                return
+            }
+            r.close()
+            stmt0.close()
+            var cancelstatus = 0
+            val stmt = con.prepareStatement("SELECT id_tender, date_version FROM ${Prefix}tender WHERE purchase_number = ? AND cancel=0 AND type_fz = ?")
+            stmt.setString(1, purNum)
+            stmt.setInt(2, typeFz)
+            val rs = stmt.executeQuery()
+            while (rs.next()) {
+                val idT = rs.getInt(1)
+                val dateB: Timestamp = rs.getTimestamp(2)
+                if (startDate.after(dateB) || dateB == Timestamp(startDate.time)) {
+                    val preparedStatement = con.prepareStatement("UPDATE ${Prefix}tender SET cancel=1 WHERE id_tender = ?")
+                    preparedStatement.setInt(1, idT)
+                    preparedStatement.execute()
+                    preparedStatement.close()
+                } else {
+                    cancelstatus = 1
+                }
+
+            }
+            rs.close()
+            stmt.close()
+            var IdOrganizer = 0
+            val fullnameOrg = html.selectFirst("div:contains(Заказчик:) + div")?.ownText()?.trim { it <= ' ' } ?: ""
+            if (fullnameOrg != "") {
+                val stmto = con.prepareStatement("SELECT id_organizer FROM ${Prefix}organizer WHERE full_name = ?")
+                stmto.setString(1, fullnameOrg)
+                val rso = stmto.executeQuery()
+                if (rso.next()) {
+                    IdOrganizer = rso.getInt(1)
+                    rso.close()
+                    stmto.close()
+                } else {
+                    rso.close()
+                    stmto.close()
+                    val phone = html.selectFirst("div:contains(Контактный телефон:) + div")?.text()?.trim { it <= ' ' }
+                            ?: ""
+                    val email = html.selectFirst("div:contains(Контактный e-mail:) + div")?.text()?.trim { it <= ' ' }
+                            ?: ""
+                    val stmtins = con.prepareStatement("INSERT INTO ${Prefix}organizer SET full_name = ?, contact_email = ?, contact_phone = ?", Statement.RETURN_GENERATED_KEYS)
+                    stmtins.setString(1, fullnameOrg)
+                    stmtins.setString(2, email)
+                    stmtins.setString(3, phone)
+                    stmtins.executeUpdate()
+                    val rsoi = stmtins.generatedKeys
+                    if (rsoi.next()) {
+                        IdOrganizer = rsoi.getInt(1)
+                    }
+                    rsoi.close()
+                    stmtins.close()
+                }
+            }
+            var IdEtp = 0
+            val etpName = "ГАЗПРОМ НЕФТЬ"
+            val etpUrl = "http://zakupki.gazprom-neft.ru/"
+            try {
+                val stmto = con.prepareStatement("SELECT id_etp FROM ${Prefix}etp WHERE name = ? AND url = ? LIMIT 1")
+                stmto.setString(1, etpName)
+                stmto.setString(2, etpUrl)
+                val rso = stmto.executeQuery()
+                if (rso.next()) {
+                    IdEtp = rso.getInt(1)
+                    rso.close()
+                    stmto.close()
+                } else {
+                    rso.close()
+                    stmto.close()
+                    val stmtins = con.prepareStatement("INSERT INTO ${Prefix}etp SET name = ?, url = ?, conf=0", Statement.RETURN_GENERATED_KEYS)
+                    stmtins.setString(1, etpName)
+                    stmtins.setString(2, etpUrl)
+                    stmtins.executeUpdate()
+                    val rsoi = stmtins.generatedKeys
+                    if (rsoi.next()) {
+                        IdEtp = rsoi.getInt(1)
+                    }
+                    rsoi.close()
+                    stmtins.close()
+                }
+            } catch (ignored: Exception) {
+
+            }
+            var IdPlacingWay = 0
+            val placingWay = html.selectFirst("div:contains(Вид процедуры:) + div")?.text()?.trim { it <= ' ' }
+                    ?: ""
+            if (placingWay != "") {
+                val stmto = con.prepareStatement("SELECT id_placing_way FROM ${Prefix}placing_way WHERE name = ? LIMIT 1")
+                stmto.setString(1, placingWay)
+                val rso = stmto.executeQuery()
+                if (rso.next()) {
+                    IdPlacingWay = rso.getInt(1)
+                    rso.close()
+                    stmto.close()
+                } else {
+                    rso.close()
+                    stmto.close()
+                    val conf = getConformity(placingWay)
+                    val stmtins = con.prepareStatement("INSERT INTO ${Prefix}placing_way SET name = ?, conformity = ?", Statement.RETURN_GENERATED_KEYS)
+                    stmtins.setString(1, placingWay)
+                    stmtins.setInt(2, conf)
+                    stmtins.executeUpdate()
+                    val rsoi = stmtins.generatedKeys
+                    if (rsoi.next()) {
+                        IdPlacingWay = rsoi.getInt(1)
+                    }
+                    rsoi.close()
+                    stmtins.close()
+
+                }
+            }
+            val purObj = html.selectFirst("div:contains(Наименование закупки:) + div")?.ownText()?.trim { it <= ' ' }
+                    ?: ""
+            var idTender = 0
+            val scoringDT = html.selectFirst("div:contains(Дата вскрытия:) + div")?.ownText()?.trim { it <= ' ' } ?: ""
+            val scoringDate = getDateFromFormat(scoringDT, formatterGpn)
+            val insertTender = con.prepareStatement("INSERT INTO ${Prefix}tender SET id_region = 0, id_xml = ?, purchase_number = ?, doc_publish_date = ?, href = ?, purchase_object_info = ?, type_fz = ?, id_organizer = ?, id_placing_way = ?, id_etp = ?, end_date = ?, cancel = ?, date_version = ?, num_version = ?, notice_version = ?, xml = ?, print_form = ?, scoring_date = ?", Statement.RETURN_GENERATED_KEYS)
+            insertTender.setString(1, purNum)
+            insertTender.setString(2, purNum)
+            insertTender.setTimestamp(3, Timestamp(startDate.time))
+            insertTender.setString(4, url)
+            insertTender.setString(5, purObj)
+            insertTender.setInt(6, typeFz)
+            insertTender.setInt(7, IdOrganizer)
+            insertTender.setInt(8, IdPlacingWay)
+            insertTender.setInt(9, IdEtp)
+            insertTender.setTimestamp(10, Timestamp(endDate.time))
+            insertTender.setInt(11, cancelstatus)
+            insertTender.setTimestamp(12, Timestamp(startDate.time))
+            insertTender.setInt(13, 1)
+            insertTender.setString(14, status)
+            insertTender.setString(15, url)
+            insertTender.setString(16, url)
+            insertTender.setTimestamp(17, Timestamp(scoringDate.time))
+            insertTender.executeUpdate()
+            val rt = insertTender.generatedKeys
+            if (rt.next()) {
+                idTender = rt.getInt(1)
+            }
+            rt.close()
+            insertTender.close()
+            AddTenderGpn++
+        })
     }
 }

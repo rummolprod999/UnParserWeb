9dc31cbb94de46f3e356bf3b1dd7edd33e151af0
rummolprod999 <rummolprod999@gmail.com>
2018-02-08 15:06:42 +0300

    update

--- src/main/java/enterit/download_tools.kt
+++ src/main/java/enterit/download_tools.kt
@@ -1,11 +1,14 @@
 package enterit
 
-import java.io.InputStreamReader
 import java.io.BufferedReader
 import java.io.InputStream
+import java.io.InputStreamReader
 import java.lang.Thread.sleep
 import java.net.URL
-import java.util.concurrent.*
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
 
 
 fun downloadFromUrl(urls: String): String {
@@ -22,7 +25,7 @@ fun downloadFromUrl(urls: String): String {
             val task = { downloadWaitWithRef(urls) }
             val future = executor.submit(task)
             try {
-                s =  future.get(60, TimeUnit.SECONDS)
+                s = future.get(60, TimeUnit.SECONDS)
             } catch (ex: TimeoutException) {
                 throw ex
             } catch (ex: InterruptedException) {
@@ -70,7 +73,7 @@ fun downloadWaitWithRef(urls: String): String {
     val s = StringBuilder()
     val url = URL(urls)
     val uc = url.openConnection()
-    uc.addRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_8; en-US) AppleWebKit/532.5 (KHTML, like Gecko) Chrome/4.0.249.0 Safari/532.5")
+    uc.addRequestProperty("User-Agent", RandomUserAgent.randomUserAgent)
     uc.connect()
     val `is`: InputStream = uc.getInputStream()
     val br = BufferedReader(InputStreamReader(`is`))

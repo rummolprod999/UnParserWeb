47dcc995693372c611c8ce54788cf00b5217203f
rummolprod999 <rummolprod999@gmail.com>
2018-02-20 14:26:21 +0300

    update

--- src/main/java/enterit/parsers/ParserRts.kt
+++ src/main/java/enterit/parsers/ParserRts.kt
@@ -3,12 +3,13 @@ package enterit.parsers
 import enterit.downloadFromUrl
 import enterit.logger
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
 
 class ParserRts : Iparser {
-    private val baseUrl = "http://corp.rts-tender.ru/?FilterData.PageSize=100&FilterData.PageIndex=1&FilterData.PageCount="
+    private val baseUrl = "http://corp.rts-tender.ru/?FilterData.PageSize=100&FilterData.PageIndex=1&FilterData.PageCount=1"
     private val maxPage = 2
     override fun parser() = (1..maxPage)
-            .map { "$baseUrl$it" }
+            .map { "http://corp.rts-tender.ru/?FilterData.PageSize=100&FilterData.PageIndex=$it&FilterData.PageCount=1" }
             .forEach {
                 try {
                     parserPage(it)
@@ -24,9 +25,17 @@ class ParserRts : Iparser {
             return
         }
         val html = Jsoup.parse(stPage)
-        val tenders = html.select("#Result table tbody tr:contains(Номер на площадке)")
+        val tenders = html.select("table.purchase-card")
         if (tenders.isEmpty()) {
             logger("Gets empty list tenders", url)
+        }
+        tenders.forEach<Element> { t ->
+            try {
+                val urlT = t.selectFirst("span a")?.attr("href")?.trim { it <= ' ' } ?: ""
+                println(urlT)
+            } catch (e: Exception) {
+                logger("error in ParserRts.parserPage()", e.stackTrace, e)
+            }
         }
     }
 }
